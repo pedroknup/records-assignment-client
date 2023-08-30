c18a29d50afb7a4a42b7668ac440c561751b8429
Pedro Knup <pedro.knup@mediamonks.com>
2023-08-30 13:15:22 +0200

    fix: fix file drag and drop bug
    Implement snackbar for error feedback

--- src/components/file-select/FileSelect.tsx
+++ src/components/file-select/FileSelect.tsx
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
+import { Alert, Snackbar } from '@mui/material';
 import './FileSelect.css';
+
+const UNSUPPORTED_FILE_FORMAT_MESSAGE: string =
+  'One or more selected files are not supported. Please select only .csv or .xml files.';
 
 interface FileSelectProps {
   onFilesChange: (files: File[]) => void;
@@ -10,6 +14,17 @@ interface FileSelectProps {
 const FileSelect: React.FC<FileSelectProps> = ({ onFilesChange, files }) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
   const [isDragging, setIsDragging] = useState(false);
+  const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener('dragenter', handleDragEnter);
+    document.addEventListener('dragover', handleDragOver);
+
+    return () => {
+      document.removeEventListener('dragenter', handleDragEnter);
+      document.removeEventListener('dragover', handleDragOver);
+    };
+  }, []);
 
   const handleDragDropClick = () => {
     inputRef.current?.click();
@@ -29,17 +44,17 @@ const FileSelect: React.FC<FileSelectProps> = ({ onFilesChange, files }) => {
     onFilesChange(files?.filter((f) => f !== file) ?? null);
   };
 
-  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
+  const handleDragOver = (event: DragEvent) => {
+    event.preventDefault();
   };
 
-  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    const newFiles = [...e.dataTransfer.files];
-    console.log(
-      'file names',
-      newFiles.map((file) => file.name)
-    );
+  const handleDrop = (event: any) => {
+    event.preventDefault();
+    if (!event?.dataTransfer?.files) {
+      return;
+    }
+
+    const newFiles = [...event.dataTransfer.files];
     const supportedFileFormats = ['csv', 'xml'];
     const unsupportedFiles = newFiles.filter(
       (file) =>
@@ -48,65 +63,67 @@ const FileSelect: React.FC<FileSelectProps> = ({ onFilesChange, files }) => {
         )
     );
     if (unsupportedFiles.length > 0) {
-      alert(
-        `The following files are not supported: ${unsupportedFiles
-          .map((file) => file.name)
-          .join(', ')}`
-      );
+      setIsSnackbarOpen(true);
       setIsDragging(false);
+      setTimeout(() => {
+        setIsSnackbarOpen(false);
+      }, 3000);
       return;
     }
-    onFilesChange(newFiles);
+
+    const fileSet = new Set([...files, ...newFiles]);
+    onFilesChange(Array.from(fileSet));
     setIsDragging(false);
   };
 
-  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
+  const handleDragEnter = (event: any) => {
+    event.preventDefault();
     setIsDragging(true);
   };
 
-  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
+  const handleDragLeave = (event: any) => {
+    event.preventDefault();
     setIsDragging(false);
   };
 
-  // useEffect(() => {
-  //   console.log('isDragging', isDragging);
-  // }, [isDragging]);
+  const closeSnackbar = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
 
-  if (files && files.length > 0)
-    return (
-      <div className="files-container">
-        {files.map((file) => (
-          <div className="file-container">
-            <div className="file-name">{file.name}</div>
-            <CloseIcon
-              className="close-icon"
-              onClick={() => removeFile(file)}
-            />
-          </div>
-        ))}
-      </div>
-    );
+    setIsSnackbarOpen(false);
+  };
 
   return (
     <div className="file-select">
-      <div
-        className="file-drop-area"
-        onDragEnter={handleDragEnter}
-        onDragLeave={handleDragLeave}
-        onDragOver={handleDragOver}
-        onDrop={handleDrop}
-      ></div>
-      <div onClick={handleDragDropClick} className="drag-drop-container">
-        <img className="cloud-icon" src="/cloud-icon.png" alt="cloud icon" />
-        <div className="drag-drop-text">
-          <span>
-            Drag and drop or <span className="fake-link">browse</span> your
-            files
-          </span>
+      {files?.length > 0 && (
+        <div className="files-container">
+          {files.map((file) => (
+            <div className="file-container">
+              <div className="file-name">{file.name}</div>
+              <CloseIcon
+                className="close-icon"
+                onClick={() => removeFile(file)}
+              />
+            </div>
+          ))}
         </div>
-      </div>
+      )}
+
+      {files?.length === 0 && (
+        <div onClick={handleDragDropClick} className="drag-drop-container">
+          <img className="cloud-icon" src="/cloud-icon.png" alt="cloud icon" />
+          <div className="drag-drop-text">
+            <span>
+              Drag and drop or <span className="fake-link">browse</span> your
+              files
+            </span>
+          </div>
+        </div>
+      )}
       <input
         accept=".csv, .xml"
         id="contained-button-file"
@@ -116,10 +133,26 @@ const FileSelect: React.FC<FileSelectProps> = ({ onFilesChange, files }) => {
         onChange={handleFileChange}
         ref={inputRef}
       />
+      <Snackbar
+        open={isSnackbarOpen}
+        autoHideDuration={6000}
+        onClose={closeSnackbar}
+      >
+        <Alert severity="error" onClose={closeSnackbar}>
+          {UNSUPPORTED_FILE_FORMAT_MESSAGE}
+        </Alert>
+      </Snackbar>
       <div className={`drop-area ${isDragging ? 'dragging' : ''}`}>
-        <div className="drop-area-text">
-          <h1>Drag and drop files here</h1>
-        </div>
+        <h1 className="drop-area-text">
+          Drag and drop files here
+        </h1>
+        {isDragging && (
+          <div
+            className="file-drop-area"
+            onDragLeave={handleDragLeave}
+            onDrop={handleDrop}
+          ></div>
+        )}
       </div>
     </div>
   );
