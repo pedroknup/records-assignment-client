777e9eee8818e013b9f9c9de0fa3fa523cc806c6
Pedro Knup <pedro.knup@mediamonks.com>
2023-08-30 13:30:10 +0200

    feat: improve error feedback
    Implement spinning loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,35 @@ import { useState } from 'react';
 import RecordsTable from './components/records-table/RecordsTable';
 import { processRecords } from './services/records.service';
 import { RecordModel } from './data/models/record.model';
-import { Button } from '@mui/material';
-import './App.css';
+import { Alert, Button, CircularProgress, Snackbar } from '@mui/material';
 import FileSelect from './components/file-select/FileSelect';
+import './App.css';
 
 function App() {
   const [records, setRecords] = useState<RecordModel[]>([]);
   const [files, setFiles] = useState<File[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   const handleFileUploadClick = async () => {
     if (!files) return;
 
-    const records = await processRecords(files);
-    setRecords(records);
+    setIsLoading(true);
+    try {
+      const records = await processRecords(files);
+      setRecords(records);
+    } catch (e: any) {
+      console.error(e);
+      setError(e.message);
+      setRecords([]);
+    }
+    setIsLoading(false);
   };
- 
+
+  const clearError = () => {
+    setError(null);
+  };
+
   return (
     <div className="container">
       <div className="form">
@@ -26,14 +40,26 @@ function App() {
             variant="contained"
             onClick={handleFileUploadClick}
             type="button"
-            disabled={files.length === 0}
+            disabled={isLoading || files.length === 0}
             className="process-files-button"
           >
+            {isLoading && (
+              <CircularProgress className="spinning-loader" size={24} />
+            )}
             Process Files
           </Button>
         </div>
       </div>
       <RecordsTable records={records} />
+      <Snackbar
+        open={error !== null}
+        autoHideDuration={6000}
+        onClose={clearError}
+      >
+        <Alert severity="error" onClose={clearError}>
+          {error}
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
