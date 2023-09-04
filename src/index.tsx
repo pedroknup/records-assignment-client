9962b9c963a8e9277c0dd79b60059446893c765b
Pedro Knup <phknup@gmail.com>
2023-09-04 12:52:54 +0200

    fix: fix App component import in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './report-web-vitals';
-import App from './app';
+import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(
